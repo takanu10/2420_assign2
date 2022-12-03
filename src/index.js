5cd32aa0f80e98a02105117837be478f1f4e7031
Darren Cheng <darrencheng10@gmail.com>
2022-12-02 18:46:41 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/api', async (request, reply) => {
+  return { hello: 'Server 1' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 5050 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
